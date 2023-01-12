c64a151654768066bb615db47644f6f16464fbc2
itaki <mritaki@gmail.com>
2023-01-11 23:24:03 -0500

    Fixed LayerShuffler

--- ref/code_snippits.py
+++ ref/code_snippits.py
@@ -6,3 +6,13 @@ iconFolder = os.path.join(my_path, "path_from_this_file_to_the_image")
 
 #to swap out new node with regular nuke node
 nuke.menu( 'Nodes' ).addCommand( 'Image/Constant', "nuke.createNode( 'ConstantPro' )")
+
+
+# to add unicode to a gizmo
+'''<span style='font-size:45px;'>&#x1F334;</span>
+
+<b><span style="font-size:49px">Color Picker</span><span style="font-size:18px"> by Lundy Hu</span></b>
+
+'''
+# Use HTML
+
